9b6305dcb02aff55e9a531b7cabb0667728992bd
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-30 12:08:44 +0400

    add index

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View ,Text , StyleSheet } from "react-native";
+
+export default function App(){
+    return(
+        <View>
+            <Text>Masser App</Text>
+        </View>
+    )
+}
