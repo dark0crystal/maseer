7ca3a6004b97086e5c4f3b538307d4f445b8c0c7
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-30 20:55:12 +0400

    importing fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,41 @@
 import {View , Text} from "react-native"
-import {Slot} from "expo-router"
+import {Slot, SplashScreen} from "expo-router"
 import React from "react"
 import 'react-native-reanimated';
+import {useFonts} from "expo-font"
+import { useEffect } from "react";
+
+//this will prevent the splash screen from auto hiding before the assests loading is complete
+SplashScreen.preventAutoHideAsync();
 
 
 
 export default function RootLayout(){
+
+
+    const [fontsLoaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+      });
+      
+      useEffect(() => {
+        if (error) throw error;
+      
+        if (fontsLoaded) {
+          SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded, error]);
+      
+      if (!fontsLoaded && !error) {
+        return null;
+      }
     return(
         <View>
             <Text className="text-5xl">welcome</Text>
