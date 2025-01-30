ce218e7e4cba8d39e60f756c8b33084383899a8d
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-30 21:38:23 +0400

    adding link

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View>
 
       <Text className="text-red-400 text-5xl ">Native wind is here</Text>
+      <Link href="/home" className="text-blue-600 bg-slate-300 rounded-lg p-2">Goo to Home</Link>
     </View>
   );
 }
