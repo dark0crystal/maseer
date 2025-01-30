67637b607ed09687987316df4aff41c7132f9174
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-30 17:29:09 +0400

    design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,17 +2,13 @@ import {View , Text} from "react-native"
 import {Slot} from "expo-router"
 import React from "react"
 import 'react-native-reanimated';
-import "./global.css";
+
 
 
 export default function RootLayout(){
     return(
-        <>
-            <Text className="text-green-500" >Hea435154der</Text>
-            <Text >Header</Text>
-            <Text className="text-violet-600 text-5xl">Header</Text>
-            <Slot/>
-            <Text className="text-5xl text-red-600">footer</Text>
-        </>
+        <View>
+                <Text className="text-5xl">oman</Text>
+        </View>
     )
 }
