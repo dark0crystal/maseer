5519a4ee8b34a7985e16932dc77484a533cb4bec
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-30 14:58:19 +0400

    add styling for test

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View>
       <Text className="text-6xl">Maseer Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quam commodi eius sapiente, quaerat atque! Quibusdam, reiciendis, quasi ipsum consectetur animi culpa mollitia labore, minima fuga molestiae nobis praesentium! Magni! App</Text> 
+
+      <Text className="text-red-500 text-2xl">Native wind is here</Text>
     </View>
   );
 }
