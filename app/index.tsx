60762f6a1eed0e0b976dcd7945741dbdf9806356
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-30 14:25:36 +0400

    fix

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function App() {
   return (
     <View>
-      <Text>Maseer Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quam commodi eius sapiente, quaerat atque! Quibusdam, reiciendis, quasi ipsum consectetur animi culpa mollitia labore, minima fuga molestiae nobis praesentium! Magni! App</Text> 
+      <Text className="text-6xl">Maseer Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quam commodi eius sapiente, quaerat atque! Quibusdam, reiciendis, quasi ipsum consectetur animi culpa mollitia labore, minima fuga molestiae nobis praesentium! Magni! App</Text> 
     </View>
   );
 }
