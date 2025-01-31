227ba990447e3d53a63c345f89aa6266d1ab0b35
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 10:38:08 +0400

    SafeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
 
       <Text className="text-red-400 text-5xl ">Native wind is here</Text>
       <Link href="/home" className="text-blue-600 bg-slate-300 rounded-lg p-2">Goo to Home</Link>
-    </View>
+    </SafeAreaView>
   );
 }
