4303d4e09dd918de460a1f37ff4bfa1c3ec4678f
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 17:22:50 +0400

    importing component

--- app/(tabs)/experience.tsx
+++ app/(tabs)/experience.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import ExperienceCards from '@/components/cards/ExperienceCards'
 
 const Experience = () => {
   return (
     <SafeAreaView>
       <Text>Experience</Text>
+      <ExperienceCards/>
     </SafeAreaView>
   )
 }
