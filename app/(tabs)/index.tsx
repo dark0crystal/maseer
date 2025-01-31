96d1667bb5883677168edca00aeff800a79a6194
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 11:48:00 +0400

    styling the category list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,35 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, FlatList } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-
-const activityCategories= [{id:"1" , name:"Scuba diving", icon:""} ,{id:"2", name:"Fishing", icon:""},{id:"3" , name:"Sea tour", icon:""} ,{id:"4" , name:"snorking", icon:""}, {id:"5" , name:"Jet ski", icon:""} ,{id:"6" , name:"Kayak", icon:""},]
+const activityCategories = [
+  { id: '1', name: 'Scuba diving' },
+  { id: '2', name: 'Fishing' },
+  { id: '3', name: 'Sea tour' },
+  { id: '4', name: 'Snorkeling' },
+  { id: '5', name: 'Jet ski' },
+  { id: '6', name: 'Kayak' },
+];
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-      <FlatList 
-      data={activityCategories}
-      keyExtractor={(category)=>category.id}
-      renderItem={({item})=>(
-        <View>
-          <Text>{item.id}</Text>
-          <Text>{item.name}</Text>
-        </View>
-      )}
+    <SafeAreaView className="flex-1 p-4 bg-white">
+      <Text className="text-2xl font-bold mb-4">Home</Text>
+      <FlatList
+        data={activityCategories}
+        keyExtractor={(category) => category.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 10 }}
+        renderItem={({ item }) => (
+          <View className="bg-gray-200 w-20 h-20 p-2 mx-2 rounded-lg items-center ">
+            <Text className="text-lg font-semibold text-gray-800">{item.id}</Text>
+            <Text className="text-sm text-gray-600 mt-1">{item.name}</Text>
+          </View>
+        )}
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
