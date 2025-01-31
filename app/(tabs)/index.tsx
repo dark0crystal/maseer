cc21f40c7e9ee076e87f6404a492ee9ddb8485c1
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 15:39:39 +0400

    use the component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
 import { View, Text, FlatList } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import ActivityCategories from '@/components/cards/ActivityCategories';
 
 
 
 const Home = () => {
   return (
-    <SafeAreaView className="flex-1 p-4 bg-white">
-      <Text className="text-2xl font-bold mb-4">Home</Text>
+    <SafeAreaView className="flex flex-col p-4 bg-white">
+      <Text className="text-2xl font-bold mb-4">Experience Oman</Text>
+      <ActivityCategories/>
      
     </SafeAreaView>
   );
