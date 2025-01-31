f902775570218c64bafd95355cfd33af62b5fb31
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 16:40:56 +0400

    new

---
+++ app/(tabs)/experience.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Experience = () => {
+  return (
+    <SafeAreaView>
+      <Text>Experience</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Experience
