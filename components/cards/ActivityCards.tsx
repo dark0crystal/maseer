6796148c985765477d39c3f62b9f2571435b8c1e
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 16:30:55 +0400

    new component

---
+++ components/cards/ActivityCards.tsx
@@ -0,0 +1,33 @@
+import { View, Text, FlatList, Image } from 'react-native';
+import {images} from "../../constants"
+export default function ActivityCards(){
+
+
+    const activityCategories = [
+        { id: '1', name: 'Scuba diving' },
+        { id: '2', name: 'Fishing' },
+        { id: '3', name: 'Sea tour' },
+        { id: '4', name: 'Snorkeling' },
+        { id: '5', name: 'Jet ski' },
+        { id: '6', name: 'Kayak' },
+    ];
+
+    return(
+        <View>
+            <Text className='text-xl font-psemibold'>Activity Cards</Text>
+             <FlatList
+                data={activityCategories}
+                keyExtractor={(category) => category.id}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 10 }}
+                renderItem={({ item }) => (
+                <View className="bg-gray-200 w-[280px] h-[310px] p-2 mx-2 rounded-lg items-center ">
+                    <Text className="text-lg font-semibold text-gray-800">{item.id}</Text>
+                    <Text className="text-sm text-gray-600 mt-1">{item.name}</Text>
+                    <Image className='w-[270px] h-[300px]' resizeMode='contain' source={images.brand}/>
+                </View>
+                )}
+            />
+        </View>
+    )
+}
