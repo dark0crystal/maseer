585493615b9b4f322e7d87c623a66b9479b48c08
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 11:32:26 +0400

    adding flat list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+const activityCategories= [{id:"1" , name:"Scuba diving", icon:""} ,{id:"2", name:"Fishing", icon:""},{id:"3" , name:"Sea tour", icon:""} ,{id:"4" , name:"snorking", icon:""}, {id:"5" , name:"Jet ski", icon:""} ,{id:"6" , name:"Kayak", icon:""},]
 
 const Home = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Home</Text>
-    </View>
+      <FlatList 
+      data={activityCategories}
+      keyExtractor={(category)=>category.id}
+      renderItem={({item})=>(
+        <View>
+          <Text>{item.id}</Text>
+          <Text>{item.name}</Text>
+        </View>
+      )}
+      />
+    </SafeAreaView>
   )
 }
 
