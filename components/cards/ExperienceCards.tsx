e24b47e2d36d8e01dc82ea48805cb548840c0280
ALMardasDEV <albusaidi9094@gmail.com>
2025-01-31 17:55:04 +0400

    fix content

--- components/cards/ExperienceCards.tsx
+++ components/cards/ExperienceCards.tsx
@@ -1,15 +1,16 @@
 import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
 import { images } from "@/constants";
+import React from "react";
 
 export default function ExperienceCards() {
   const activityCategories = [
-    { id: "1", name: "Scuba Diving", description: "Explore underwater beauty", image: images.brand },
-    { id: "2", name: "Fishing", description: "Relax and catch some fish", image:images.brand },
-    { id: "3", name: "Sea Tour", description: "Enjoy a scenic boat tour", image: images.brand },
-    { id: "4", name: "Snorkeling", description: "Discover marine life up close", image: images.brand },
-    { id: "5", name: "Jet Ski", description: "Feel the thrill on water", image: images.brand },
-    { id: "6", name: "Kayak", description: "Paddle through calm waters", image: images.brand },
+    { id: "1", name: "Scuba Diving", image: images.brand },
+    { id: "2", name: "Fishing", image: images.kayak },
+    { id: "3", name: "Sea Tour", image: images.brand },
+    { id: "4", name: "Snorkeling", image: images.brand },
+    { id: "5", name: "Jet Ski",  image: images.brand },
+    { id: "6", name: "Kayak", image: images.brand },
   ];
 
   return (
@@ -22,19 +23,19 @@ export default function ExperienceCards() {
         contentContainerStyle={{ gap: 10 }}
         renderItem={({ item }) => (
           <Link href="/" asChild>
-            <TouchableOpacity className="w-48 h-64 rounded-xl overflow-hidden relative">
-              {/* صورة الغلاف */}
-              <Image source={item.image} className="w-full h-full absolute" resizeMode="cover" />
-              
-              {/* التعتيم فوق الصورة */}
-              <View className="absolute inset-0 bg-black opacity-30" />
-              
-              {/* محتوى البطاقة */}
-              <View className="absolute bottom-4 left-4">
-                <Text className="text-white text-xl font-bold">{item.name}</Text>
-                <Text className="text-white text-sm">{item.description}</Text>
+            <View className="w-48">
+              {/* الصورة */}
+              <TouchableOpacity className="h-64 rounded-xl overflow-hidden relative">
+                <Image source={item.image} className="w-full h-full absolute" resizeMode="cover" />
+                {/* تأثير التعتيم */}
+                <View className="absolute inset-0 bg-black opacity-30" />
+              </TouchableOpacity>
+
+              {/* النصوص أسفل الصورة */}
+              <View className="mt-1  flex flex-col items-center">
+                <Text className="text-black text-base font-pregular">{item.name}</Text>
               </View>
-            </TouchableOpacity>
+            </View>
           </Link>
         )}
       />
