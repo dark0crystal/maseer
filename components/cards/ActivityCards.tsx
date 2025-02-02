da26f909db28828ce029ae8e40876dbbd4c3fc07
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-02 09:41:57 +0400

    design the cards

--- components/cards/ActivityCards.tsx
+++ components/cards/ActivityCards.tsx
@@ -4,12 +4,12 @@ export default function ActivityCards(){
 
 
     const activityCategories = [
-        { id: '1', name: 'Scuba diving', type:"Hard" , female:false ,price:"100.0",companyName:"MfqodLTD" },
-        { id: '2', name: 'Fishing' , type:"Easy" , female:true ,price:"1200",companyName:"Masser"},
-        { id: '3', name: 'Sea tour' , type:"Mid" , female:false ,price:"20",companyName:"Masser Almotahidah"},
-        { id: '4', name: 'Snorkeling' , type:"Hard" , female:false,price:"30.5",companyName:"Masseeer trips" },
-        { id: '5', name: 'Jet ski' , type:"Easy" , female:true ,price:"30000",companyName:"Oman trips LTD shshssshs"},
-        { id: '6', name: 'Kayak' , type:"Mid" , female:false ,price:"49.5",companyName:"MfqodLTD"},
+        { id: '1', location:"Muscat", title: 'Scuba diving', type:"Hard" , female:false ,price:"100.0",companyName:"MfqodLTD" },
+        { id: '2', location:"Bidyah", title: 'Fishing' , type:"Easy" , female:true ,price:"1200",companyName:"Masser"},
+        { id: '3', location:"Sidab", title: 'Sea tour' , type:"Mid" , female:false ,price:"20",companyName:"Masser Almotahidah"},
+        { id: '4', location:"Qatar", title: 'Snorkeling' , type:"Hard" , female:false,price:"30.5",companyName:"Masseeer trips" },
+        { id: '5', location:"Saudi", title: 'Jet ski' , type:"Easy" , female:true ,price:"30000",companyName:"Oman trips LTD shshssshs"},
+        { id: '6', location:"Dubai", title: 'Kayak' , type:"Mid" , female:false ,price:"49.5",companyName:"MfqodLTD"},
     ];
 
     return(
@@ -21,13 +21,22 @@ export default function ActivityCards(){
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ paddingHorizontal: 10  ,alignItems: "center",}}
                 renderItem={({ item }) => (
-                <View className="bg-black-200/30 w-[330px] h-[350px] p-4 m-2 rounded-3xl items-center border border-red-300">
-                    <View className='relative w-full h-[300px] overflow-hidden rounded-3xl'>
-                        <Image className='absolute w-full h-full' resizeMode="contain" source={images.brand}/>
+                <View className="bg-white shadow w-[330px] h-[350px] p-3 m-2 rounded-3xl items-center border border-red-300 flex flex-col overflow-hidden">
+                    {/* Image section */}
+                    <View className='relative w-full h-[230px] overflow-hidden rounded-2xl border '>
+                        <Image className='absolute w-full h-full' resizeMode="cover" source={images.brand}/>
                     </View>
-                    <Text className="text-lg font-semibold text-gray-800">{item.id}</Text>
-                    <Text className="text-sm text-gray-600 mt-1">{item.name}</Text>
-                   
+                    {/* content section */}
+                    <View className='bg-violet-300 w-full p-2 mt-1 overflow-hidden rounded-2xl border flex flex-row'>
+                        <View>
+                            <Text className="text-sm text-gray-600 mt-1">{item.title}</Text>
+                            <Text className="text-sm text-gray-600 mt-1">{item.location}</Text>location
+                            <Text className="text-sm text-gray-600 mt-1">{item.price} R.O</Text>
+                        </View>
+                        <View>
+                             <Text className="text-sm text-gray-600 mt-1">{item.type}</Text>
+                        </View>
+                    </View>
                 </View>
                 )}
             />
