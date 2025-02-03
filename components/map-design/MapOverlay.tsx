bcce3ca093e72d3cd1a735af3e9c0609d0246c92
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 17:58:00 +0400

    fix overlay design

--- components/map-design/MapOverlay.tsx
+++ components/map-design/MapOverlay.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function MapOverlay(){
     return(
-        <View className="absolute w-full h-[30vh] bg-white">
+        <View className="absolute w-fit h-[20vh] rounded-3xl bg-white bottom-20 left-10 right-10">
             <Text className="text-black text-2xl">New Activity</Text>
         </View>
     )
