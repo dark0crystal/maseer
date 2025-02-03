67442bbc2b47332a0d064652a82cd0807b070e01
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 15:42:04 +0400

    add data

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -5,6 +5,53 @@ import { Stack } from 'expo-router';
 import {Marker} from 'react-native-maps';
 
 export default function App() {
+
+    const data = [
+        {
+          "latitude": 23.6143,
+          "longitude": 58.5453,
+          "activityType": "Hiking",
+          "price": "Free",
+          "title": "Muttrah Mountain Trail"
+        },
+        {
+          "latitude": 22.9333,
+          "longitude": 57.5333,
+          "activityType": "Camping",
+          "price": "5 OMR per night",
+          "title": "Wadi Bani Khalid Camp"
+        },
+        {
+          "latitude": 20.5328,
+          "longitude": 56.9417,
+          "activityType": "Diving",
+          "price": "30 OMR",
+          "title": "Dimaniyat Islands Diving"
+        },
+        {
+          "latitude": 23.3941,
+          "longitude": 57.6669,
+          "activityType": "Rock Climbing",
+          "price": "15 OMR",
+          "title": "Jebel Akhdar Climbing"
+        },
+        {
+          "latitude": 17.0197,
+          "longitude": 54.0897,
+          "activityType": "Beach Relaxation",
+          "price": "Free",
+          "title": "Mughsail Beach Getaway"
+        },
+        {
+          "latitude": 23.3000,
+          "longitude": 58.8833,
+          "activityType": "Desert Safari",
+          "price": "50 OMR",
+          "title": "Sharqiya Sands Adventure"
+        }
+      ]
+      
+
   return (
     <View style={styles.container}>
         <Stack.Screen options={{headerShown:false}}/> 
