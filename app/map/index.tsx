cf8be3511593ed12a9f09ca5dabfeb82ab69c8a5
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 15:13:09 +0400

    hide the header

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { Stack } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
+        <Stack.Screen options={{headerShown:false}}/> 
       <MapView style={styles.map} />
     </View>
   );
