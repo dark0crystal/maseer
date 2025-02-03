76d1285d37559a922c5e7c145a2c8fb943274a5a
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 17:52:07 +0400

    add content

---
+++ components/map-design/MapOverlay.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function MapOverlay(){
+    return(
+        <View className="absolute w-full h-[30vh] bg-white">
+            <Text className="text-black text-2xl">New Activity</Text>
+        </View>
+    )
+}
