d28aae72ce800e6e49f7f43f1d9faf3251a24851
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 18:07:29 +0400

    fix content

--- components/map-design/MapOverlay.tsx
+++ components/map-design/MapOverlay.tsx
@@ -1,9 +1,47 @@
-import { Text, View } from "react-native";
+import { View, Text, FlatList, Image } from 'react-native';
+import {images} from "../../constants"
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Link } from "expo-router";
+import { StarRatingDisplay} from 'react-native-star-rating-widget';
+
 
 export default function MapOverlay(){
     return(
-        <View className="absolute w-fit h-[20vh] rounded-3xl bg-white bottom-20 left-10 right-10">
-            <Text className="text-black text-2xl">New Activity</Text>
-        </View>
+        <Link href="/map"  className='mt-2'>
+                <View className="bg-white shadow w-[350px] h-[190px] p-2 m-2 rounded-3xl   border-red-300 flex flex-row overflow-hidden gap-1 ">
+                    {/* image section */}
+                   <View className="relative  w-[120px] h-[160px] overflow-hidden rounded-2xl ">
+                        <Image className="absolute w-full h-full" resizeMode="cover" source={images.kayak} />   
+                    </View>
+
+                    {/* content section */}
+                    <View className='bg-white w-[200px]  h-[160px]  p-2  overflow-hidden rounded-2xl  flex flex-row '>
+
+                        
+                        {/* Left section */}
+                        <View>
+                            <Text className="text-base font-psemibold text-gray-600 ">oman</Text>
+                            <Text className="text-base text-gray-600 mt-1"><Ionicons name="location-outline" size={13} color="black" />location</Text>
+                            <View className=''>
+                                <StarRatingDisplay
+                                    rating={2.5}
+                                    starSize={18}
+                                />
+                            </View>
+                            <Text className="text-base font-pmedium text-gray-600 mt-1">10000</Text>
+                            <View className='flex flex-row items-center  gap-1'>
+                                <View className='relative flex flex-row items-center justify-center'>
+                                    <View className='absolute h-4 w-4 bg-green-300 rounded-full'/>
+                                    <View className='absolute h-3 w-3 bg-green-400 rounded-full'/>
+                                    <View className='h-2 w-2 bg-green-500 rounded-full'/>
+                                    
+                                </View>
+                                <Text className="text-sm text-gray-600"> jjjfa</Text>
+                                
+                             </View>
+                        </View>
+                    </View>
+                </View>
+                </Link>
     )
 }
