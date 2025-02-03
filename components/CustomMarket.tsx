a9284ff187f6c45977e270bf7e418b29a35b803f
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 17:39:42 +0400

    add custom marker

---
+++ components/CustomMarket.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View ,Text} from 'react-native';
+import { Link, Stack } from 'expo-router';
+import {Marker} from 'react-native-maps';
+
+export default function CustomMarker({activity}){
+    return(
+
+        <Marker 
+            key={index}
+            coordinate={{latitude: activity.latitude , longitude: activity.longitude}}
+            title={activity.title}
+            description={activity.price}>
+            <Link href={`/details/${activity.id}`} className='mt-2'>
+                <View className='bg-white border p-2 rounded-2xl'>
+                    <Text className='text-sm font-pmedium'>{activity.activityType}</Text>  
+                </View>
+            </Link>
+         
+        </Marker>
+    )
+}
