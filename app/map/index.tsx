983ccf24a63c9ecbd1bc8d87b350842fde8ee93b
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 16:02:48 +0400

    add data

--- app/map/index.tsx
+++ app/map/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
-import { Stack } from 'expo-router';
+import { StyleSheet, View ,Text} from 'react-native';
+import { Link, Stack } from 'expo-router';
 import {Marker} from 'react-native-maps';
 
 export default function App() {
 
     const data = [
         {
-          "latitude": 23.6143,
+          "id":1,
+          "latitude": 23.4543,
           "longitude": 58.5453,
           "activityType": "Hiking",
           "price": "Free",
           "title": "Muttrah Mountain Trail"
         },
         {
+          "id":2,
           "latitude": 22.9333,
           "longitude": 57.5333,
           "activityType": "Camping",
@@ -22,6 +24,7 @@ export default function App() {
           "title": "Wadi Bani Khalid Camp"
         },
         {
+          "id":3,
           "latitude": 20.5328,
           "longitude": 56.9417,
           "activityType": "Diving",
@@ -29,13 +32,15 @@ export default function App() {
           "title": "Dimaniyat Islands Diving"
         },
         {
+          "id":4,
           "latitude": 23.3941,
-          "longitude": 57.6669,
+          "longitude": 55.6669,
           "activityType": "Rock Climbing",
           "price": "15 OMR",
           "title": "Jebel Akhdar Climbing"
         },
         {
+          "id":5,
           "latitude": 17.0197,
           "longitude": 54.0897,
           "activityType": "Beach Relaxation",
@@ -43,7 +48,8 @@ export default function App() {
           "title": "Mughsail Beach Getaway"
         },
         {
-          "latitude": 23.3000,
+          "id":6,
+          "latitude": 21.3000,
           "longitude": 58.8833,
           "activityType": "Desert Safari",
           "price": "50 OMR",
@@ -56,33 +62,29 @@ export default function App() {
     <View style={styles.container}>
         <Stack.Screen options={{headerShown:false}}/> 
       <MapView 
-       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      style={styles.map} >
-         <Marker
-            coordinate={{latitude:37.78825 , longitude:-122.4324}}
-            title="Helllo there"
-            description="let's goooo"
-        />
-        <Marker
-            coordinate={{latitude:35.78825 , longitude:-122.4324}}
-            title="Helllo there"
-            description="let's goooo"
-        />
-        <Marker
-            coordinate={{latitude:37.78825 , longitude:-112.4324}}
-            title="Helllo there"
-            description="let's goooo"
-        />
-        <Marker
-            coordinate={{latitude:37.78825 , longitude:-127.4324}}
-            title="Helllo there"
-            description="let's goooo"
-        />
+    //    initialRegion={{
+    //     latitude: 37.78825,
+    //     longitude: -122.4324,
+    //     latitudeDelta: 0.0922,
+    //     longitudeDelta: 0.0421,
+    //   }}
+      style={styles.map}
+       >
+        {data.map((marker,index)=>(
+            <Marker 
+            key={index}
+            coordinate={{latitude: marker.latitude , longitude: marker.longitude}}
+            title={marker.title}
+            description={marker.price}>
+                <Link href={`/details/${marker.id}`} className='mt-2'>
+                    <View className='bg-violet-200 p-2 rounded-2xl'>
+                        <Text className='font-pmedium'>{marker.activityType}</Text>  
+                    </View>
+                </Link>
+             
+            </Marker>
+
+        ))}
       </MapView>
     </View>
   );
