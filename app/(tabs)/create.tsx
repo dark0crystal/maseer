effe1b242679a39d54405ba79ec4dca2ce98d1cf
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-03 13:44:43 +0400

    adding the cards in the create page

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import DiscoverCards from '@/components/cards/DiscoverCards'
 
 const Create = () => {
   return (
     <SafeAreaView>
-      <Text>Create</Text>
+      <Text>Discover</Text>
+      <DiscoverCards/>
     </SafeAreaView>
   )
 }
