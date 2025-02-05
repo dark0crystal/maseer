5c58fda66c61e729b8bbf27602aaee182d7b1918
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:14:34 +0400

    remove unused

--- app/(admin)/index.tsx
+++ app/(admin)/index.tsx
@@ -1,16 +1,16 @@
-import { useAddNewActivity } from "@/store/FormStore";
-import { View, Text, Button } from "react-native";
+// import { useAddNewActivity } from "@/store/FormStore";
+// import { View, Text, Button } from "react-native";
 
-export default function Home() {
-  const price = useAddNewActivity((state) => state.price);
-  const increase = useAddNewActivity((state) => state.incresePrice); // Fixed function name
-  const decrease = useAddNewActivity((state) => state.decreasePrice);
+// export default function Home() {
+//   const price = useAddNewActivity((state) => state.price);
+//   const increase = useAddNewActivity((state) => state.incresePrice); // Fixed function name
+//   const decrease = useAddNewActivity((state) => state.decreasePrice);
 
-  return (
-    <View>
-      <Text>{price}</Text>
-      <Button title="Increase" onPress={increase} /> {/* Fixed Button */}
-      <Button title="Decrease" onPress={decrease} /> {/* Fixed Button */}
-    </View>
-  );
-}
+//   return (
+//     <View>
+//       <Text>{price}</Text>
+//       <Button title="Increase" onPress={increase} /> {/* Fixed Button */}
+//       <Button title="Decrease" onPress={decrease} /> {/* Fixed Button */}
+//     </View>
+//   );
+// }
