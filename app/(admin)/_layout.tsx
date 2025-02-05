3a68b386eeb76969ade03f7d3b1b0cfd5bc552ab
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 17:13:59 +0400

    add new stack

--- app/(admin)/_layout.tsx
+++ app/(admin)/_layout.tsx
@@ -7,6 +7,7 @@ export default function Create() {
       <Stack.Screen name="StepOne" options={{ title: "Step 1" }} />
       <Stack.Screen name="StepTwo" options={{ title: "Step 2" }} />
       <Stack.Screen name="StepThree" options={{ title: "Step 3" }} />
+      <Stack.Screen name="StepFour" options={{ title: "Step 4" }} />
     </Stack>
   );
 }
