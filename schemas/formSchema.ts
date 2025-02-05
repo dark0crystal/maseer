2c56d7c916811cacc961fdf18a74777dc186778f
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 16:55:06 +0400

    validate type

--- schemas/formSchema.ts
+++ schemas/formSchema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const formSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
+  activityType: z.string().min(1, "Please select an activity type"),
   description: z.string().min(10, "Description must be at least 10 characters"),
   price: z.string().regex(/^\d+$/, "Price must be a number"),
   features: z.array(z.string()).min(1, "Select at least one feature"),
