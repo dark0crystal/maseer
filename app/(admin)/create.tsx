1530e44edb2b3eb60967b6aa147e2375d140ab8d
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:48:16 +0400

    fix

--- app/(admin)/create.tsx
+++ app/(admin)/create.tsx
@@ -1,12 +1,13 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
+// import StepOne from "./StepOne";
 
-export default function Create() {
-  return (
-    <Stack>
-      <Stack.Screen name="/MultiStepForm/screens/StepOne" options={{ title: "Step 1" }} />
-      <Stack.Screen name="step2" options={{ title: "Step 2" }} />
-      <Stack.Screen name="step3" options={{ title: "Step 3" }} />
-    </Stack>
-  );
-}
+// export default function Create() {
+//   return (
+//     <Stack>
+//       <Stack.Screen name="StepOne" options={{ title: "Step 1" }} />
+//       <Stack.Screen name="StepTwo" options={{ title: "Step 2" }} />
+//       <Stack.Screen name="StepThree" options={{ title: "Step 3" }} />
+//     </Stack>
+//   );
+// }
 
