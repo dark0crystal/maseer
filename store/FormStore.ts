a89fb095d0f870136007cd936c864682edac9cfb
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 11:56:23 +0400

    using zustand

--- store/FormStore.ts
+++ store/FormStore.ts
@@ -1,6 +1,13 @@
 import { create } from 'zustand'
 
 type FormFields ={
-    title :string 
     price:number 
+    incresePrice :()=> void
+    decreasePrice :()=> void
 }
+
+export const useAddNewActivity  = create<FormFields>((set)=>({
+    price:0,
+    incresePrice: ()=>{set((state)=>({price :state.price+1}));},
+    decreasePrice: ()=>{set((state)=>({price :state.price-1}));},
+}))
