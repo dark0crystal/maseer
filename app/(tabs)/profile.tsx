b5fd9706ec1f5e41277cff2b3018d3c53f61762a
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 10:40:50 +0400

    add link to admin

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
 
 const Profile = () => {
   return (
     <SafeAreaView>
       <Text>Profile</Text>
+
+      <Link href="/(admin)/create">Go to admin</Link>
     </SafeAreaView>
   )
 }
