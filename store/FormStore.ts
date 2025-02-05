e26c6974e0cbd2feaedd1331d556c08bf525995e
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 14:36:21 +0400

    add the description to the store

--- store/FormStore.ts
+++ store/FormStore.ts
@@ -2,10 +2,12 @@ import { create } from "zustand";
 
 interface FormState {
   title: string;
+  description:string;
   price: string;
   features: string[];
   images: string[];
   setTitle: (title: string) => void;
+  setDescription:(description: string) => void;
   setPrice: (price: string) => void;
   setFeatures: (features: string[]) => void;
   setImages: (images: string[]) => void;
@@ -13,10 +15,12 @@ interface FormState {
 
 export const useFormStore = create<FormState>((set) => ({
   title: "",
+  description : "",
   price: "",
   features: [],
   images: [],
   setTitle: (title) => set({ title }),
+  setDescription:(description) => set({ description }),
   setPrice: (price) => set({ price }),
   setFeatures: (features) => set({ features }),
   setImages: (images) => set({ images }),
