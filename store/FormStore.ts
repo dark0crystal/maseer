d95ed552405950298d69c8a6e8cf0795b2a9d550
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:03:02 +0400

    add new zustand store

--- store/FormStore.ts
+++ store/FormStore.ts
@@ -1,13 +1,23 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
-type FormFields ={
-    price:number 
-    incresePrice :()=> void
-    decreasePrice :()=> void
+interface FormState {
+  title: string;
+  price: string;
+  features: string[];
+  images: string[];
+  setTitle: (title: string) => void;
+  setPrice: (price: string) => void;
+  setFeatures: (features: string[]) => void;
+  setImages: (images: string[]) => void;
 }
 
-export const useAddNewActivity  = create<FormFields>((set)=>({
-    price:0,
-    incresePrice: ()=>{set((state)=>({price :state.price+1}));},
-    decreasePrice: ()=>{set((state)=>({price :state.price-1}));},
-}))
+export const useFormStore = create<FormState>((set) => ({
+  title: "",
+  price: "",
+  features: [],
+  images: [],
+  setTitle: (title) => set({ title }),
+  setPrice: (price) => set({ price }),
+  setFeatures: (features) => set({ features }),
+  setImages: (images) => set({ images }),
+}));
