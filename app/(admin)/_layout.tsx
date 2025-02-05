ff3dbd5f32f3d00ca9a09e23a4d742f9ff312869
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:48:07 +0400

    add layout

---
+++ app/(admin)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+
+export default function Create() {
+  return (
+    <Stack>
+      <Stack.Screen name="StepOne" options={{ title: "Step 1" }} />
+      <Stack.Screen name="StepTwo" options={{ title: "Step 2" }} />
+      <Stack.Screen name="StepThree" options={{ title: "Step 3" }} />
+    </Stack>
+  );
+}
