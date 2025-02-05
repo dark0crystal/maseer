0b81434c50f4258298c096ec4b6fa5be115b8d52
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:14:23 +0400

    fix

--- app/(admin)/create.tsx
+++ app/(admin)/create.tsx
@@ -1,10 +1,9 @@
 import { Stack } from "expo-router";
 
-export default function Layout() {
+export default function Create() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="step1" options={{ title: "Step 1" }} />
+      <Stack.Screen name="/MultiStepForm/screens/StepOne" options={{ title: "Step 1" }} />
       <Stack.Screen name="step2" options={{ title: "Step 2" }} />
       <Stack.Screen name="step3" options={{ title: "Step 3" }} />
     </Stack>
