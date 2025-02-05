77a5c5afeddb4e0ce858693018cadc882c64aebf
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:02:29 +0400

    use expo router

--- app/(admin)/create.tsx
+++ app/(admin)/create.tsx
@@ -1,22 +1,13 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import StepOne from "../../components/MultiStepForm/screens/StepOne";
-import StepTwo from "../../components/MultiStepForm/screens/StepTwo";
-import StepThree from "../../components/MultiStepForm/screens/StepThree";
-import StepFour from "../../components/MultiStepForm/screens/StepFour";
+import { Stack } from "expo-router";
 
-const Stack = createStackNavigator();
-
-export default function App() {
+export default function Layout() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="StepOne">
-        <Stack.Screen name="StepOne" component={StepOne} options={{ title: "Step 1: Details" }} />
-        <Stack.Screen name="StepTwo" component={StepTwo} options={{ title: "Step 2: Features" }} />
-        <Stack.Screen name="StepThree" component={StepThree} options={{ title: "Step 3: Images" }} />
-        <Stack.Screen name="StepFour" component={StepFour} options={{ title: "Step 4: Review & Submit" }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="step1" options={{ title: "Step 1" }} />
+      <Stack.Screen name="step2" options={{ title: "Step 2" }} />
+      <Stack.Screen name="step3" options={{ title: "Step 3" }} />
+    </Stack>
   );
 }
+
