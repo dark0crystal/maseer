3a64d1d853260bf995c8a0f7892c1fa14a7b6e25
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-05 12:02:37 +0400

    fix

--- app/(admin)/index.tsx
+++ app/(admin)/index.tsx
@@ -1,18 +1,16 @@
 import { useAddNewActivity } from "@/store/FormStore";
-import { View ,Text, Button} from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default function Home(){
+export default function Home() {
+  const price = useAddNewActivity((state) => state.price);
+  const increase = useAddNewActivity((state) => state.incresePrice); // Fixed function name
+  const decrease = useAddNewActivity((state) => state.decreasePrice);
 
-    const price = useAddNewActivity((state)=>state.price)
-    const increase = useAddNewActivity((state)=>state.incresePrice)
-    const decrease = useAddNewActivity((state)=>state.decreasePrice)
-    return(
-        <View>
-            <Text>
-                {price}
-            </Text>
-            <Button onPress={increase}>increase</Button>
-            <Button onPress={decrease}>decrease</Button>
-        </View>
-    )
+  return (
+    <View>
+      <Text>{price}</Text>
+      <Button title="Increase" onPress={increase} /> {/* Fixed Button */}
+      <Button title="Decrease" onPress={decrease} /> {/* Fixed Button */}
+    </View>
+  );
 }
