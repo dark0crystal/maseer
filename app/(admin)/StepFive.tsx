d75fccc08202ee3b0fe6462cd7ba1378f14af619
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-06 17:19:23 +0400

    import nessecery

--- app/(admin)/StepFive.tsx
+++ app/(admin)/StepFive.tsx
@@ -1,4 +1,9 @@
-import { View } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { formSchema } from "../../schemas/formSchema";
+import { useFormStore } from "../../store/FormStore";
+import { useRouter } from "expo-router";
 
 export default function StepFour(){
     return(
