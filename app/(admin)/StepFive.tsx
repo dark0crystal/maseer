ed27a897eef49a89e8863febae3eaa373b963516
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-06 17:38:00 +0400

    fix

--- app/(admin)/StepFive.tsx
+++ app/(admin)/StepFive.tsx
@@ -8,16 +8,15 @@ import { useRouter } from "expo-router";
 export default function StepFour(){
 
     const router = useRouter();
-    const { title, description ,setDescription, setTitle  } = useFormStore();
+    const { price, setPrice  } = useFormStore();
     
     const { register, handleSubmit, setValue, formState: { errors } } = useForm({
       resolver: zodResolver(formSchema.pick({ title: true, description: true })),
-      defaultValues: { title, description }
+      defaultValues: { price, setPrice }
     });
   
     const onNext = (data: any) => {
-      setTitle(data.title);
-      setDescription(data.description);
+      setPrice(data.price);
       router.push("./StepSix");
     };
 
@@ -27,9 +26,11 @@ export default function StepFour(){
             <Text className="text-base text-gray-500 mt-1">You can change it any time.</Text>
             <TextInput 
             className="text-5xl text-black "
+            {...register("price")} 
+            onChangeText={(text) => setValue("price", text)}
             />
 
-        {errors.description && <Text style={{ color: "red" }}>{errors.description.message}</Text>}
+        {errors.price && <Text style={{ color: "red" }}>{errors.price.message}</Text>}
     
     
         <View className="bg-black rounded-lg w-fit p-4 mt-6">
