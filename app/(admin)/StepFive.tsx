24993778362554f6babfd591d6f18d6a0f184164
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-06 17:23:15 +0400

    add title and description

--- app/(admin)/StepFive.tsx
+++ app/(admin)/StepFive.tsx
@@ -8,7 +8,9 @@ import { useRouter } from "expo-router";
 export default function StepFour(){
     return(
         <View>
-            
+            <Text className="text-xl font-pmedium text-black mt-4">Now, Select Your Price</Text>
+            <Text className="text-base text-gray-500 mt-1">You can change it any time.</Text>
+            <TextInput/>
         </View>
     )
 }
