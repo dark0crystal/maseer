1f56057088d5f8116596cf16ed56155d5175fba5
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-07 19:08:19 +0400

    fix

--- app/(admin)/_layout.tsx
+++ app/(admin)/_layout.tsx
@@ -10,6 +10,8 @@ export default function Create() {
       <Stack.Screen name="StepTwo" options={{ title: "Step 2"  ,headerShown:false}} />
       <Stack.Screen name="StepThree" options={{ title: "Step 3",headerShown:false }} />
       <Stack.Screen name="StepFour" options={{ title: "Step 4" ,headerShown:false}} />
+      <Stack.Screen name="StepFive" options={{ title: "Step 5",headerShown:false }} />
+      <Stack.Screen name="StepSix" options={{ title: "Step 6" ,headerShown:false}} />
     </Stack>
   );
 }
