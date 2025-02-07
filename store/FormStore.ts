2ad9ea5465d8623fb8bc60a3d919d11221e0fea1
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-07 22:06:27 +0400

    add new data to store

--- store/FormStore.ts
+++ store/FormStore.ts
@@ -18,7 +18,6 @@ interface FormState {
   availableSeats: number;
   genderPreference: "male" | "female" | "both";
   allowPets: boolean;
-  
   setTitle: (title: string) => void;
   setDescription: (description: string) => void;
   setPrice: (price: number) => void;
@@ -28,35 +27,38 @@ interface FormState {
   setGovernorate: (gov: string) => void;
   setCity: (city: string) => void;
   setCoordinates: (coordinates: Coordinates) => void;
-  setAvailableSeats: (seats: number) => void;
+//   setAvailableSeats: (seats: number) => void;
+  incrementSeat:(availableSeats:number) => void;
+  decrementSeat:(availableSeats:number) => void;
   setGenderPreference: (preference: "male" | "female" | "both") => void;
   setAllowPets: (allow: boolean) => void;
 }
 
 export const useFormStore = create<FormState>((set) => ({
-  title: "",
-  description: "",
-  price: 20,
-  features: [],
-  images: [],
-  activityType: "",
-  governorate: "",
-  city: "",
-  coordinates: null,
-  availableSeats: 0,  
-  genderPreference: "both",
-  allowPets: false,
-
-  setTitle: (title) => set({ title }),
-  setDescription: (description) => set({ description }),
-  setPrice: (price) => set({ price }),
-  setFeatures: (features) => set({ features }),
-  setImages: (images) => set({ images }),
-  setActivityType: (type) => set({ activityType: type }),
-  setGovernorate: (gov) => set({ governorate: gov }),
-  setCity: (city) => set({ city }),
-  setCoordinates: (coordinates) => set({ coordinates }),
-  setAvailableSeats: (seats) => set({ availableSeats: seats }),
-  setGenderPreference: (preference) => set({ genderPreference: preference }),
-  setAllowPets: (allow) => set({ allowPets: allow }),
-}));
+    title: "",
+    description: "",
+    price: 20,
+    features: [],
+    images: [],
+    activityType: "",
+    governorate: "",
+    city: "",
+    coordinates: null,
+    availableSeats: 0,
+    genderPreference: "both",
+    allowPets: false,
+    setTitle: (title) => set({ title }),
+    setDescription: (description) => set({ description }),
+    setPrice: (price) => set({ price }),
+    setFeatures: (features) => set({ features }),
+    setImages: (images) => set({ images }),
+    setActivityType: (type) => set({ activityType: type }),
+    setGovernorate: (gov) => set({ governorate: gov }),
+    setCity: (city) => set({ city }),
+    setCoordinates: (coordinates) => set({ coordinates }),
+    incrementSeat: () => set((state) => ({ availableSeats: state.availableSeats + 1 })),
+    decrementSeat: () => set((state) => ({ availableSeats: Math.max(0, state.availableSeats - 1) })),
+    setGenderPreference: (preference) => set({ genderPreference: preference }),
+    setAllowPets: (allow) => set({ allowPets: allow }),
+  }));
+  
