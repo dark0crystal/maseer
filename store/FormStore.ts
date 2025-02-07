747c774330544469d739481b6e4531a82221d740
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-07 21:36:35 +0400

    add the new values to the store

--- store/FormStore.ts
+++ store/FormStore.ts
@@ -1,13 +1,13 @@
 import { create } from "zustand";
 
 interface Coordinates {
-    latitude: number;
-    longitude: number;
-  }
+  latitude: number;
+  longitude: number;
+}
 
 interface FormState {
   title: string;
-  description:string;
+  description: string;
   price: number;
   activityType: string;
   features: string[];
@@ -15,8 +15,12 @@ interface FormState {
   governorate: string;
   city: string;
   coordinates: Coordinates | null;
+  availableSeats: number;
+  genderPreference: "male" | "female" | "both";
+  allowPets: boolean;
+  
   setTitle: (title: string) => void;
-  setDescription:(description: string) => void;
+  setDescription: (description: string) => void;
   setPrice: (price: number) => void;
   setFeatures: (features: string[]) => void;
   setImages: (images: string[]) => void;
@@ -24,25 +28,35 @@ interface FormState {
   setGovernorate: (gov: string) => void;
   setCity: (city: string) => void;
   setCoordinates: (coordinates: Coordinates) => void;
+  setAvailableSeats: (seats: number) => void;
+  setGenderPreference: (preference: "male" | "female" | "both") => void;
+  setAllowPets: (allow: boolean) => void;
 }
 
 export const useFormStore = create<FormState>((set) => ({
   title: "",
-  description : "",
+  description: "",
   price: 20,
   features: [],
   images: [],
   activityType: "",
+  governorate: "",
+  city: "",
+  coordinates: null,
+  availableSeats: 0,  
+  genderPreference: "both",
+  allowPets: false,
+
   setTitle: (title) => set({ title }),
-  setDescription:(description) => set({ description }),
+  setDescription: (description) => set({ description }),
   setPrice: (price) => set({ price }),
   setFeatures: (features) => set({ features }),
   setImages: (images) => set({ images }),
   setActivityType: (type) => set({ activityType: type }),
-  governorate: "",
-  city: "",
-  coordinates: null,
   setGovernorate: (gov) => set({ governorate: gov }),
   setCity: (city) => set({ city }),
   setCoordinates: (coordinates) => set({ coordinates }),
+  setAvailableSeats: (seats) => set({ availableSeats: seats }),
+  setGenderPreference: (preference) => set({ genderPreference: preference }),
+  setAllowPets: (allow) => set({ allowPets: allow }),
 }));
