9181d30a01b03116881769a2634afc69a7ed18e0
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-07 10:15:39 +0400

    fix

--- app/(admin)/_layout.tsx
+++ app/(admin)/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
+import { View, Text, TextInput, Keyboard, TouchableWithoutFeedback } from "react-native";
 
 
 export default function Create() {
   return (
     <Stack>
+        
       <Stack.Screen name="StepOne" options={{ title: "Step 1" }} />
       <Stack.Screen name="StepTwo" options={{ title: "Step 2" }} />
       <Stack.Screen name="StepThree" options={{ title: "Step 3" }} />
