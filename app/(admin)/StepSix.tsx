3532b92d436f3328e957e7488271186c5742d4c9
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-07 10:15:24 +0400

    keyboard fix

--- app/(admin)/StepSix.tsx
+++ app/(admin)/StepSix.tsx
@@ -1,9 +1,11 @@
-import { View } from "react-native";
+import { View ,Keyboard, TouchableWithoutFeedback} from "react-native";
 
 export default function StepSix(){
     return(
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View>
             Step Six
         </View>
+        </TouchableWithoutFeedback>
     )
 }
