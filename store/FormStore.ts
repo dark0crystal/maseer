b5426b5c58cfea04bfd87d530ec1e28ae6c3f638
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-07 10:03:35 +0400

    fix

--- store/FormStore.ts
+++ store/FormStore.ts
@@ -1,5 +1,10 @@
 import { create } from "zustand";
 
+interface Coordinates {
+    latitude: number;
+    longitude: number;
+  }
+
 interface FormState {
   title: string;
   description:string;
@@ -7,12 +12,18 @@ interface FormState {
   activityType: string;
   features: string[];
   images: string[];
+  governorate: string;
+  city: string;
+  coordinates: Coordinates | null;
   setTitle: (title: string) => void;
   setDescription:(description: string) => void;
   setPrice: (price: number) => void;
   setFeatures: (features: string[]) => void;
   setImages: (images: string[]) => void;
   setActivityType: (type: string) => void;
+  setGovernorate: (gov: string) => void;
+  setCity: (city: string) => void;
+  setCoordinates: (coordinates: Coordinates) => void;
 }
 
 export const useFormStore = create<FormState>((set) => ({
@@ -28,4 +39,10 @@ export const useFormStore = create<FormState>((set) => ({
   setFeatures: (features) => set({ features }),
   setImages: (images) => set({ images }),
   setActivityType: (type) => set({ activityType: type }),
+  governorate: "",
+  city: "",
+  coordinates: null,
+  setGovernorate: (gov) => set({ governorate: gov }),
+  setCity: (city) => set({ city }),
+  setCoordinates: (coordinates) => set({ coordinates }),
 }));
