fd7b37b3f7e8cb0cf62117fbd232b6fdc845bb75
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-10 15:36:02 +0400

    new content

---
+++ app/details/ReservationStepOne.tsx
@@ -0,0 +1,12 @@
+import { View ,Text} from "react-native";
+
+
+export default function ReservationStepOne(){
+    return(
+        <View>
+            <Text>
+                Step one
+            </Text>
+        </View>
+    )
+}
