127290037aca757b3f7f6911d7e8b1b9137ba830
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-10 15:35:56 +0400

    new content

---
+++ app/details/ReservationMain.tsx
@@ -0,0 +1,12 @@
+import { View ,Text} from "react-native";
+
+
+export default function ReservationMain(){
+    return(
+        <View>
+            <Text>
+                Step Main
+            </Text>
+        </View>
+    )
+}
