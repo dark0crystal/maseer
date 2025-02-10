e21070d4c5f582c34b6f8f5e3294bd0e4c240445
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-10 12:20:19 +0400

    add content

--- app/reservation.tsx
+++ app/reservation.tsx
@@ -1,13 +1,35 @@
-import { View, Text } from "react-native";
+
 import { useLocalSearchParams } from "expo-router";
+import { View, Text, TouchableOpacity, TextInput, Keyboard, TouchableWithoutFeedback, ScrollView , Image} from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import MapView, { Marker } from "react-native-maps";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import { images } from "../constants";
+
 
 export default function Reservation() {
   const { id } = useLocalSearchParams(); // Get activity ID from URL
 
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-xl font-bold">Reservation Page</Text>
-      <Text className="text-lg mt-4">Activity ID: {id}</Text>
-    </View>
+    
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View className="flex-1 bg-gray-100">
+            <ScrollView
+                className="bg-white p-4"
+                contentContainerStyle={{ flexGrow: 1, paddingBottom: 150 }}
+                keyboardShouldPersistTaps="handled"
+            >
+            {/* Activity image */}
+            <View className="relative w-full h-72 rounded-lg overflow-hidden">
+                <Image source={images.kayak} width={200} height={200} resizeMode="cover" />
+            </View>
+            <View className="h-[100vh]"/>
+            
+            </ScrollView>
+        </View>
+    </TouchableWithoutFeedback>
   );
 }
