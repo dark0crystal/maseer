6ce6395ab2a6bae8441e17b593018bd0d6d85e22
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-11 21:30:33 +0400

    fix schema

--- schemas/reservationSchema.ts
+++ schemas/reservationSchema.ts
@@ -2,12 +2,10 @@ import { z } from "zod";
 
 
 export const reservationSchema = z.object({
-  dates: z
-    .array(
-      z.object({
+    dates: z
+      .object({
         start: z.string(),
         end: z.string(),
       })
-    )
-    .min(1, "Please select at least one date range"),
-});
+      .nullable(),
+  });
