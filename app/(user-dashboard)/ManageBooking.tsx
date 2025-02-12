deb7897fcc0c442cca011e423a300eca2dcf1b9b
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-12 10:41:20 +0400

    add new component

---
+++ app/(user-dashboard)/ManageBooking.tsx
@@ -0,0 +1,12 @@
+import { View ,Text } from "react-native";
+
+export default function ManageBooking(){
+    return(
+        <View>
+            <Text>
+                Manage Booking
+            </Text>
+          
+        </View>
+    )
+}
