ec4dca40cb5306b8c2f8ef3144b35ccd11e0e5f7
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-26 22:10:32 +0400

    progress bar component

---
+++ components/shared-components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+// components/ProgressBar.tsx
+import { View } from "react-native";
+import { useFormStore } from "../../store/FormStore";
+
+const TOTAL_STEPS = 8;
+
+export default function ProgressBar() {
+  const { formprogress } = useFormStore();
+
+  // Calculate the width percentage based on the current step
+  const progressWidth = `${(formprogress / (TOTAL_STEPS - 1)) * 100}%` as const;
+
+  return (
+    <View className="h-2 w-full bg-gray-200 border">
+      <View
+        className="h-2 bg-black"
+        style={{ width: progressWidth }} // TypeScript now accepts this
+      />
+    </View>
+  );
+}
