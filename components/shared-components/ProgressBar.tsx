21cbdf071a12804f0aba0f35fc87029e369a7e4b
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-27 14:06:02 +0400

    fix the progress bar

--- components/shared-components/ProgressBar.tsx
+++ components/shared-components/ProgressBar.tsx
@@ -9,12 +9,13 @@ export default function ProgressBar() {
 
   // Calculate the width percentage based on the current step
   const progressWidth = `${(formprogress / (TOTAL_STEPS - 1)) * 100}%` as const;
+  console.log(progressWidth)
 
   return (
     <View className="h-2 w-full bg-gray-200 border">
       <View
-        className="h-2 bg-black"
-        style={{ width: progressWidth }} // TypeScript now accepts this
+        className={`h-2 bg-black w-[${progressWidth}]`}
+        
       />
     </View>
   );
