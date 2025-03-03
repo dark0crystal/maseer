a24084416fbb2caf767c45c5654df64cf9c53498
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-03 11:43:24 +0400

    testing output

--- app/(admin)/StepEight.tsx
+++ app/(admin)/StepEight.tsx
@@ -1,7 +1,15 @@
 import { View ,Text } from "react-native";
+import { useFormStore } from "../../store/FormStore";
+
+
 
 export default function StepEight(){
-    
+    const { title, features, coverImage, images, price,description ,coordinates,availableSeats, genderPreference,activityDates} = useFormStore();
+
+    console.log("in eight")
+    console.log({ title, description ,features, coverImage, images,price , coordinates,availableSeats, genderPreference,activityDates});
+
+
 
     return(
         <View>
