cd8a6aac65f55d6f049d7c7f66c075347dd58fb5
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-05 00:22:38 +0400

    remove unused

--- app/(user-dashboard)/Currencychange.tsx
+++ app/(user-dashboard)/Currencychange.tsx
@@ -5,17 +5,8 @@ import { useEffect,useState } from "react";
 
 export default function CurrencyChange(){
    
-      const { currency, setCurrency, convertPrice } = useCurrencyStore(); // Access Zustand store
-        const basePrice = 20; // Original price in OMR
-      
-        const [convertedPrice, setConvertedPrice] = useState(convertPrice(basePrice));
-      
-        // Load saved currency when the component mounts
-        useEffect(() => {
-          setConvertedPrice(convertPrice(basePrice));
-        }, [currency]);
-      
-
+     
+    const { currency,setCurrency, convertPrice } = useCurrencyStore(); // Access Zustand store
 
     return(
         <View>
@@ -26,7 +17,7 @@ export default function CurrencyChange(){
               selectedValue={currency}
               onValueChange={async (newCurrency) => {
                 await setCurrency(newCurrency);
-                setConvertedPrice(convertPrice(basePrice)); // Update price when currency changes
+               
               }}
             >
               <Picker.Item label="OMR - Omani Rial" value="OMR" />
