adc3d492e9ee5c7d405045b6e52a115babc95bef
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-07 13:19:54 +0400

    style the profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -3,24 +3,24 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Link } from 'expo-router'
 import CurrencyChange from '../(user-dashboard)/Currencychange'
-import Auth from '@/components/auth/Auth'
 import Account from '@/components/auth/Account'
 
 const Profile = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white p-4">
       {/* Make the content scrollable */}
-      <ScrollView contentContainerStyle={{ padding: 16 }}>
-        <Text>Profile</Text>
+      <ScrollView className="space-y-6">
+        <Text className="text-2xl font-semibold text-gray-800">Profile</Text>
 
-        <Link href="/(admin)/StepOne">Add New Activity</Link>
+        <Link href="/(admin)/StepOne" className="text-blue-500 text-lg">Add New Activity</Link>
 
         {/* show this link after checking if the user is a manager of activity */}
-        <Link href="/(user-dashboard)/manage-activity">Show Activities</Link>
+        <Link href="/(user-dashboard)/manage-activity" className="text-blue-500 text-lg">Show Activities</Link>
 
-        <Link href="/(admin)/">Go to index</Link>
+        <Link href="/(admin)/" className="text-blue-500 text-lg">Go to index</Link>
+
         <CurrencyChange />
-        <Account/>
+        <Account />
       </ScrollView>
     </SafeAreaView>
   )
