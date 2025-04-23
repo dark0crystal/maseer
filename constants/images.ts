91b95c9f4f3c00baf84011a86ee8857a7bd46906
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-23 11:59:20 +0400

    fix

--- constants/images.ts
+++ constants/images.ts
@@ -9,5 +9,9 @@
 // export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
 import brand from "../assets/images/brand.jpg";
 import kayak from "../assets/images/kayak.jpeg"
+import caravan from "../assets/images/caravan.jpeg"
+import tent from "../assets/images/tent.jpeg"
+import tent2 from "../assets/images/tent2.jpeg"
+import tent3 from "../assets/images/tent3.jpeg"
 
-export default { brand ,kayak };
+export default { brand ,kayak ,caravan ,tent ,tent2 ,tent3};
