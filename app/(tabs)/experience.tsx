64144a480b51c142282ee3a0c8ef7f6367fda1b7
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-24 10:40:30 +0400

    add transtaltion

--- app/(tabs)/experience.tsx
+++ app/(tabs)/experience.tsx
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ExperienceCards from '@/components/cards/ExperienceCards'
-
+import { useTranslation } from 'react-i18next'
 
 const Experience = () => {
+  const { t } = useTranslation("experience");
+  
   return (
-    <SafeAreaView>
-      <Text>Experience</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <Text className="text-2xl font-bold p-4">{t('experience')}</Text>
       <ExperienceCards/>
     </SafeAreaView>
   )
